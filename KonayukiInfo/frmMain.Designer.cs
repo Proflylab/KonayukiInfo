57c9145d5993735f9d1dac2476d0f07571fceb61
Proflylab <proflylab@protonmail.com>
2018-03-16 22:14:06 +0700

    First Release

---
+++ KonayukiInfo/frmMain.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace KonayukiInfo
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.pnInfo = new System.Windows.Forms.Panel();
+            this.rtbInfo = new System.Windows.Forms.RichTextBox();
+            this.btnOk = new MetroFramework.Controls.MetroButton();
+            this.btnExport = new MetroFramework.Controls.MetroButton();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.pnInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnInfo
+            // 
+            this.pnInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnInfo.BackColor = System.Drawing.Color.White;
+            this.pnInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnInfo.Controls.Add(this.rtbInfo);
+            this.pnInfo.Location = new System.Drawing.Point(20, 45);
+            this.pnInfo.Name = "pnInfo";
+            this.pnInfo.Size = new System.Drawing.Size(677, 500);
+            this.pnInfo.TabIndex = 69;
+            // 
+            // rtbInfo
+            // 
+            this.rtbInfo.BackColor = System.Drawing.Color.White;
+            this.rtbInfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbInfo.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbInfo.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.rtbInfo.Location = new System.Drawing.Point(0, 0);
+            this.rtbInfo.Name = "rtbInfo";
+            this.rtbInfo.ReadOnly = true;
+            this.rtbInfo.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.rtbInfo.Size = new System.Drawing.Size(675, 498);
+            this.rtbInfo.TabIndex = 67;
+            this.rtbInfo.Text = "";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOk.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.btnOk.Location = new System.Drawing.Point(621, 560);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 70;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseSelectable = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExport.Location = new System.Drawing.Point(540, 560);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 71;
+            this.btnExport.Text = "EXPORT";
+            this.btnExport.UseSelectable = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // lblFile
+            // 
+            this.lblFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblFile.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFile.ForeColor = System.Drawing.Color.Plum;
+            this.lblFile.Location = new System.Drawing.Point(18, 563);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(516, 17);
+            this.lblFile.TabIndex = 72;
+            this.lblFile.Text = "?";
+            this.lblFile.UseMnemonic = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = MetroFramework.Forms.MetroFormBorderStyle.FixedSingle;
+            this.ClientSize = new System.Drawing.Size(720, 600);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.pnInfo);
+            this.DisplayHeader = false;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(720, 600);
+            this.Name = "frmMain";
+            this.Padding = new System.Windows.Forms.Padding(20, 30, 20, 20);
+            this.ShadowType = MetroFramework.Forms.MetroFormShadowType.None;
+            this.Style = MetroFramework.MetroColorStyle.Purple;
+            this.Text = "KONAYUKI INFO";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.pnInfo.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnInfo;
+        private System.Windows.Forms.RichTextBox rtbInfo;
+        private MetroFramework.Controls.MetroButton btnOk;
+        private MetroFramework.Controls.MetroButton btnExport;
+        private System.Windows.Forms.Label lblFile;
+    }
+}
+
---
+++ KonayukiInfo/frmMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using MediaInfoLib;
+using MetroFramework.Forms;
+
+namespace KonayukiInfo
+{
+    public partial class frmMain : MetroForm
+    {
+        string file = string.Empty;
+        public frmMain(string file)
+        {
+            InitializeComponent();
+            this.file = file;
+        }
+        private void SetToolTip(Control control, string text)
+        {
+            ToolTip toolTip = new ToolTip();
+            toolTip.ShowAlways = true;
+            toolTip.SetToolTip(control, text);
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            MediaInfo MI = new MediaInfo();
+            MI.Open(file);
+            MI.Option("Complete", "0");
+            rtbInfo.Text = MI.Inform();
+            MI.Close();
+            lblFile.Text = Path.GetFileName(file);
+            SetToolTip(lblFile, Path.GetFileName(file));
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            var sfdExport = new SaveFileDialog
+            {
+                Title = "Export",
+                Filter = "Konayuki Info|*.txt",
+                FileName = Path.GetFileName(file) + ".txt"
+            };
+            if (sfdExport.ShowDialog(this) == DialogResult.OK)
+            {
+                File.WriteAllText(sfdExport.FileName, rtbInfo.Text.Replace("\n", "\r\n"));
+            }
+        }
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
